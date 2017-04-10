ed413ecd5de8988215810e288a34f1835c52d3d6
manuellena5 <manu_landeta@hotmail.com>
2017-04-10 02:40:21 -0300

    Creacion de form Categorias

---
+++ Club Mitre/SociosMitre/UI.Escritorio/frmActualizacionPrecios.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Entidades;
+using Negocio;
+
+namespace UI.Escritorio
+{
+    public partial class frmActualizacionPrecios : ApplicationForm
+    {
+        
+
+        #region VARIABLES
+        #endregion
+
+        #region PROPIEDADES
+        #endregion
+
+
+
+        #region CONSTRUCTOR
+
+        
+
+        public frmActualizacionPrecios()
+        {
+            InitializeComponent();
+            this.dataListado.AutoGenerateColumns = false;
+        }
+
+        #endregion
+
+
+        #region METODOS
+
+        public void listar()
+        {
+            CategoriaLogic catl = new CategoriaLogic();
+
+            this.dataListado.DataSource = catl.GetAll();
+           
+
+        }
+
+        #endregion
+
+        #region EVENTOS
+
+        private void frmActualizacionPrecios_Load(object sender, EventArgs e)
+        {
+            this.listar();
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            this.listar();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            frmAMBcategoria frm = new frmAMBcategoria(ApplicationForm.ModoForm.Alta);
+            frm.ShowDialog();
+            this.listar();
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            int ID = ((Socio)this.dataListado.SelectedRows[0].DataBoundItem).NroSocio;
+            frmAMBcategoria frm = new frmAMBcategoria(ID, ApplicationForm.ModoForm.Modificacion);
+            frm.ShowDialog();
+            this.listar();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            int ID = ((Categoria)this.dataListado.SelectedRows[0].DataBoundItem).Id_categoria;
+            frmAMBcategoria frm = new frmAMBcategoria(ID, ApplicationForm.ModoForm.Baja);
+            frm.BloquearControles(true);
+            frm.ShowDialog();
+            this.listar();
+
+        }
+
+
+       
+
+        #endregion
+
+
+    }
+}
