10636e3f54c70e92ee7b3f0bcf9a795e1789fb23
manuellena5 <manu_landeta@hotmail.com>
2017-04-26 11:35:13 -0300

    terminados los reportes

---
+++ Club Mitre/SociosMitre/UI.Escritorio/frmVistaPrevHistorialPagos.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UI.Escritorio
+{
+    public partial class frmVistaPrevHistorialPagos : Form
+    {
+        public frmVistaPrevHistorialPagos()
+        {
+            InitializeComponent();
+        }
+
+        public int anior, mesr, nro_socior;
+
+        private void frmVistaPrevHistorialPagos_Load(object sender, EventArgs e)
+        {
+            ReporteHistorialPagos rep = new ReporteHistorialPagos();
+
+            rep.SetParameterValue("@anio", anior);
+            rep.SetParameterValue("@mes", mesr);
+            rep.SetParameterValue("@nro_socio", nro_socior);
+
+            crystalReportViewer1.ReportSource = rep;
+        }
+    }
+}
---
+++ Club Mitre/SociosMitre/UI.Escritorio/frmVistaPrevListadoEstadoActualSociosActivos.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UI.Escritorio
+{
+    public partial class frmVistaPrevListadoEstadoActualSociosActivos : Form
+    {
+        public frmVistaPrevListadoEstadoActualSociosActivos()
+        {
+            InitializeComponent();
+        }
+
+        private void frmVistaPrevListadoEstadoActualSociosActivos_Load(object sender, EventArgs e)
+        {
+            ReporteEstadoActualSocios rep = new ReporteEstadoActualSocios();
+            crystalReportViewer1.ReportSource = rep;
+        }
+    }
+}
